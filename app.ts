462097b45d4e4ff9f3519b912733a58b50acdde2
Pablo <pablo.fariabritto@gmail.com>
2017-10-20 21:20:02 -0200

    Criação do repositório typescript

---
+++ app.ts
@@ -0,0 +1,8 @@
+let mensagem: string = "Seja bem vindo!!!"
+let temporadasFriends: number = 10
+let estudandoAngular: boolean = true
+let listaDeFrutas1: Array<string> = ['Uva', 'Banana', 'Abacaxi']
+let listaDeFrutas2: string['Uva', 'Banana', 'Abacaxi']
+let notasDasProvas: Array<number> = [7.5, 8, 9]
+
+mensagem = 'Oi'
---
+++ dist/app.js
@@ -0,0 +1,9 @@
+"use strict";
+var mensagem = "Seja bem vindo!!!";
+var temporadasFriends = 10;
+var estudandoAngular = true;
+var listaDeFrutas1 = ['Uva', 'Banana', 'Abacaxi'];
+var listaDeFrutas2;
+'Banana', 'Abacaxi';
+var notasDasProvas = [7.5, 8, 9];
+mensagem = 'Oi';
