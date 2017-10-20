b450b9c1d31daa515b9818fa38e9eb7470c196ca
Pablo <pablo.fariabritto@gmail.com>
2017-10-20 21:30:42 -0200

    Updates

--- app.ts
+++ app.ts
@@ -2,7 +2,6 @@ let mensagem: string = "Seja bem vindo!!!"
 let temporadasFriends: number = 10
 let estudandoAngular: boolean = true
 let listaDeFrutas1: Array<string> = ['Uva', 'Banana', 'Abacaxi']
-let listaDeFrutas2: string['Uva', 'Banana', 'Abacaxi']
-let notasDasProvas: Array<number> = [7.5, 8, 9]
-
-mensagem = 'Oi'
+let listaDeFrutas2: string[] = ['Uva', 'Banana', 'Abacaxi']
+let notasDasProvas1: Array<number> = [7.5, 8, 9]
+let notasDasProvas2: number[] = [7.5, 8, 9]
